52035e83b07bfb71f7a06a0aad19774e9a12355d
RogaJedi <7654595@gmail.com>
2025-05-19 20:59:08 +0300

    fixed home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:verstak/demo_products.dart';
 import 'package:verstak/widgets/big_card.dart';
 import 'package:verstak/widgets/product_carousel.dart';
 
 import '../api_service.dart';
+import '../product.dart';
 import '../widgets/product_card/product_card.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,69 +19,88 @@ class HomePage extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
+    return FutureBuilder<List<Product>>(
+        future: apiService.getProducts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text("Пожалуйста подождите..."));
+          }
+
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return Center(child: Text('Err: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No products'));
+          }
+
+          final products = snapshot.data!;
+
+          return Scaffold(
+            body: Column(
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    BigCard(
-                      picture: "https://cdn.mos.cms.futurecdn.net/TjtZ88nogxegChtJxdis3m-1200-80.jpg",
-                      mainText: "Пасха на handmade",
-                      subText: "Покупайте товары к празднику со скидкой",
-                      onTap: () => print("tapped"),
-                    ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          BigCard(
+                            picture: "https://cdn.mos.cms.futurecdn.net/TjtZ88nogxegChtJxdis3m-1200-80.jpg",
+                            mainText: "Пасха на handmade",
+                            subText: "Покупайте товары к празднику со скидкой",
+                            onTap: () => print("tapped"),
+                          ),
 
-                    SizedBox(height: 20,),
-                    Padding(
-                      padding: EdgeInsets.only(left: screenWidth * 0.02),
-                      child: Text(
-                        "Популярное",
-                        style: TextStyle(
-                            fontSize: 25
-                        ),
-                      ),
-                    ),
-                    ProductCarousel(
-                      apiService: apiService,
-                    ),
+                          SizedBox(height: 20,),
+                          Padding(
+                            padding: EdgeInsets.only(left: screenWidth * 0.02),
+                            child: Text(
+                              "Популярное",
+                              style: TextStyle(
+                                  fontSize: 25
+                              ),
+                            ),
+                          ),
+                          ProductCarousel(
+                            apiService: apiService,
+                          ),
 
-                    SizedBox(height: 20,),
-                    Padding(
-                      padding: EdgeInsets.only(left: screenWidth * 0.02),
-                      child: Text(
-                        "Подобрали для вас",
-                        style: TextStyle(
-                            fontSize: 25
-                        ),
+                          SizedBox(height: 20,),
+                          Padding(
+                            padding: EdgeInsets.only(left: screenWidth * 0.02),
+                            child: Text(
+                              "Подобрали для вас",
+                              style: TextStyle(
+                                  fontSize: 25
+                              ),
+                            ),
+                          ),
+                          GridView.builder(
+                            shrinkWrap: true, // Add this
+                            physics: NeverScrollableScrollPhysics(), // Add this
+                            padding: const EdgeInsets.all(8),
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 0.825,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 20,
+                            ),
+                            itemCount: products.length,
+                            itemBuilder: (context, index) {
+                              return ProductCard(product: products[index], apiService: apiService,);
+                            },
+                          ),
+                        ],
                       ),
-                    ),
-                    GridView.builder(
-                      shrinkWrap: true, // Add this
-                      physics: NeverScrollableScrollPhysics(), // Add this
-                      padding: const EdgeInsets.all(8),
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: 0.825,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 20,
-                      ),
-                      itemCount: DemoProducts.length,
-                      itemBuilder: (context, index) {
-                        return ProductCard(product: DemoProducts[index], apiService: apiService,);
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
+          );
+        }
     );
   }
 }
-
